ae0dd9fa58f1ad5e2c8e1bf7575aefa95fce105b
Javier Martín <jmartinpizarro04@gmail.com>
2024-08-12 13:16:37 +0200

    hotfix: algorithm did not work if the original message had spaces

--- src/classic_algorithms/caesar.js
+++ src/classic_algorithms/caesar.js
@@ -26,18 +26,27 @@ function caesarCipher(message, alphabet, n_rotation) {
         counter++;
     }
 
-    for (let i = 0; i < n_rotation; i++){   // for the remaining positions, we start again from the beggining
+    for (let i = 0; i < n_rotation; i++) {   // for the remaining positions, we start again from the beggining
         cipheredAlphabet.push(alphabet[i]);
     }
 
     const nomenclator = {}  // object that Object[chracter from init alphabet] -> character from ciphered alphabet
-    for (let i = 0; i < cipheredAlphabet.length; i++){
+    for (let i = 0; i < cipheredAlphabet.length; i++) {
         nomenclator[alphabet[i]] = cipheredAlphabet[i]
     }
 
-    for (index in message) {
-        cipheredMessage += nomenclator[message[index]]
+    const messageToCipher = message.replace(' ', '').trim();
+    for (index in messageToCipher) {
+        cipheredMessage += nomenclator[messageToCipher[index]]
+    }
+
+    if (cipheredMessage.length != message.length) {
+        throw new Error('caesarCipher() Internal Error: Something wrong has happened when ciphering the message: length of both, ciphered and original message is not the same')
     }
 
     return { cipheredAlphabet, cipheredMessage }     // returns the ciphered message and the rotated alphabet
 };
+
+
+const output = caesarCipher('abcd e', alphabet, 2)
+console.log(output)
