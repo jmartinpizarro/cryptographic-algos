3528c06f70f8dfeeb317dccfbe65513150102d19
Javier Martín <jmartinpizarro04@gmail.com>
2024-08-13 14:39:56 +0200

    Implement the zigzag transposition algorithm

--- src/classic_algorithms/transposition.js
+++ src/classic_algorithms/transposition.js
@@ -6,8 +6,29 @@
  * @returns 
  */
 
-function transpositionCipher(message, keyWord, model){
-    let cipherMessage = ''
+// v.1.0.0 can handles two types of transposition cipher: zigzag and single columnar transposition cipher
+function transpositionCipher(message, model = 'zigzag', keyWord = '') {
+    const messageToCipher = message.trim().replaceAll(' ', '')
+
+    if (model === 'zigzag') {
+        let oddNumbers = []
+        let evenNumbers = []
+        for (let i = 0; i < messageToCipher.length; i++) {
+            if (i % 2 == 0) {
+                evenNumbers.push(i)
+            } else {
+                oddNumbers.push(i)
+            }
+        }
+
+        let cipherMessage = ''
+        for (let i = 0; i < evenNumbers.length; i++) {
+            cipherMessage += messageToCipher[evenNumbers[i]]
+        }
+        for (let i = 0; i < oddNumbers.length; i++) {
+            cipherMessage += messageToCipher[oddNumbers[i]]
+        }
+    }
     return cipherMessage
 }
 
