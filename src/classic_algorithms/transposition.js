738cadebfab5539b024ebc8e128e2c27569e53f3
Javier Martín <jmartinpizarro04@gmail.com>
2024-08-16 17:53:22 +0200

    Refactor to switch statement

--- src/classic_algorithms/transposition.js
+++ src/classic_algorithms/transposition.js
@@ -13,60 +13,63 @@ function transpositionCipher(message, model = 'zigzag', keyWord = '') {
     }
     const messageToCipher = message.trim().replaceAll(' ', '')
     let cipherMessage = ''
-    if (model === 'zigzag') {
-        let oddNumbers = []
-        let evenNumbers = []
-        for (let i = 0; i < messageToCipher.length; i++) {
-            if (i % 2 == 0) {
-                evenNumbers.push(i)
-            } else {
-                oddNumbers.push(i)
-            }
-        }
-
-        for (let i = 0; i < evenNumbers.length; i++) {
-            cipherMessage += messageToCipher[evenNumbers[i]]
-        }
-        for (let i = 0; i < oddNumbers.length; i++) {
-            cipherMessage += messageToCipher[oddNumbers[i]]
-        }
-    } else {
-        if (model !== 'single-columnar') {
-            throw new SyntaxError('transpositionCipher() Error: Model parameter can be either "zigzag" or "single-columnar"');
-        }
-        if (typeof keyWord !== 'object' || Object.keys(keyWord).length === 0) {
-            throw new TypeError('transpositionCipher() Error: Keyword parameter must be an object such as Object<string, number> with length bigger than 0');
-        }
-
-        let matrix = [];
-        let keyLength = Object.keys(keyWord).length;
-        let rowIndex = 0;
-
-        // fill the matrix
-        for (let i = 0; i < messageToCipher.length; i += keyLength) {
-            let row = messageToCipher.slice(i, i + keyLength).split('');
-
-            while (row.length < keyLength) {
-                row.push('_');
-            }
-        
-            matrix[rowIndex] = row;
-            rowIndex++;
-        }
-
-        // order columns
-        let sortedKey = Object.entries(keyWord).sort((a, b) => a[1] - b[1]);
-
-        for (let [letter, order] of sortedKey) {
-            let columnIndex = Object.keys(keyWord).indexOf(letter);
-            for (let row of matrix) {
-                if (row[columnIndex]) {
-                    cipherMessage += row[columnIndex];
+    switch (model) {
+        case 'zigzag':
+            let oddNumbers = []
+            let evenNumbers = []
+            for (let i = 0; i < messageToCipher.length; i++) {
+                if (i % 2 == 0) {
+                    evenNumbers.push(i)
+                } else {
+                    oddNumbers.push(i)
                 }
             }
-        }
+
+            for (let i = 0; i < evenNumbers.length; i++) {
+                cipherMessage += messageToCipher[evenNumbers[i]]
+            }
+            for (let i = 0; i < oddNumbers.length; i++) {
+                cipherMessage += messageToCipher[oddNumbers[i]]
+            }
+            return cipherMessage
+        case 'single-columnar':
+            if (typeof keyWord !== 'object' || Object.keys(keyWord).length === 0) {
+                throw new TypeError('transpositionCipher() Error: Keyword parameter must be an object such as Object<string, number> with length bigger than 0');
+            }
+
+            let matrix = [];
+            let keyLength = Object.keys(keyWord).length;
+            let rowIndex = 0;
+
+            // fill the matrix
+            for (let i = 0; i < messageToCipher.length; i += keyLength) {
+                let row = messageToCipher.slice(i, i + keyLength).split('');
+
+                while (row.length < keyLength) {
+                    row.push('_');
+                }
+
+                matrix[rowIndex] = row;
+                rowIndex++;
+            }
+
+            // order columns
+            let sortedKey = Object.entries(keyWord).sort((a, b) => a[1] - b[1]);
+
+            for (let [letter, order] of sortedKey) {
+                let columnIndex = Object.keys(keyWord).indexOf(letter);
+                for (let row of matrix) {
+                    if (row[columnIndex]) {
+                        cipherMessage += row[columnIndex];
+                    }
+                }
+            }
+            return cipherMessage
+        default:
+            throw new TypeError('tranpositionCipher() Error: "model" can be either "single-columnar" or "zigzag"')
     }
-    return cipherMessage
+
+
 }
 
 module.exports = transpositionCipher
